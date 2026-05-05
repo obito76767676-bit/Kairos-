aad16c1f7d74d18f6703088e7a64cc832a277f17
hwmatwkytw-rgb <hwmatwkytw@gmail.com>
2026-05-05 04:10:38 +0200

    Update permission ID in تحديث.js

--- modules/commands/تحديث.js
+++ modules/commands/تحديث.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event, args }) {
   const fs = require("fs-extra");
-  const permission = [`61581906898524`];
+  const permission = [`100081948980908`];
   if (!permission.includes(event.senderID)) return api.sendMessage("ليس لديك إذن لاستخدام هذا الأمر.\nفقط المطور", event.threadID, event.messageID);
   let appstate = api.getAppState();
   // convert JSON object to a string
